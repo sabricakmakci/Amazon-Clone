d821cacd7f3ddee3dda91e30f1497a94606569bf
Sabri Çakmakçı <92885036+sabricakmakci@users.noreply.github.com>
2023-11-14 23:45:00 +0300

    unused import

--- lib/features/admin/screens/posts_screen.dart
+++ lib/features/admin/screens/posts_screen.dart
@@ -3,7 +3,6 @@ import 'package:amazon_clone/features/account/widgets/single_product.dart';
 import 'package:amazon_clone/features/admin/screens/add_product_screen.dart';
 import 'package:amazon_clone/features/admin/services/admin_services.dart';
 import 'package:amazon_clone/models/product.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class PostsScreen extends StatefulWidget {
